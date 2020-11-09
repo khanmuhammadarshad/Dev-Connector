aaaca75e3697c11809184dfb91f7462a89b940c1
khanmuhammadarshad <arshad.khan@intellexalsolutions.com>
2020-11-09 06:14:18 -0800

    prepared for deployment

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const connectDb = require("./config/db");
+const path = require("path");
 const app = express();
-app.use(express.json());
+app.use(express.json({ extended: false }));
 connectDb();
-app.get("/", (req, res) => res.send("api running"));
 const newLocal = "/api/user";
 app.use(newLocal, require("./routes/apis/user"));
 app.use("/api/auth", require("./routes/apis/auth"));
 app.use("/api/post", require("./routes/apis/post"));
 app.use("/api/profile", require("./routes/apis/profile"));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log("Server started on port 5001"));
